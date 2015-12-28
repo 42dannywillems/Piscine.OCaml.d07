1d2097b610c118118d500beb43f1cea451b7b0e4
Danny Willems <contact@danny-willems.be>
2015-12-28 13:09:37 +0100

    ex01 ok and tested

---
+++ ex01/main.ml
@@ -0,0 +1,6 @@
+let () =
+    let a = new Doctor.doctor "Doctor" 52 (new People.people "Doctor") in
+    print_endline a#to_string;
+    a#talk;
+    a#travel_in_time 1998 2005;
+    print_endline a#to_string
